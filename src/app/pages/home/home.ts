923393f36965adea9451b9fcc8d52ffcac023938
miguel <miguel.arellano@jaak.ai>
2026-08-04 13:53:38 -0600

    feat: the category component was added in home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Title } from "../../components/title/title";
 import { BenefitsTable } from "../../components/benefits-table/benefits-table";
+import { Category } from "../../components/category/category/category";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Title, BenefitsTable],
+  imports: [Header, Title, BenefitsTable, Category],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
