f121484db456e63923f96714375e56611d196a7d
miguel <miguel.arellano@jaak.ai>
2026-08-04 12:03:48 -0600

    feat: the new components and scss style sheets was added in home page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
+import { Title } from "../../components/title/title";
+import { BenefitsTable } from "../../components/benefits-table/benefits-table";
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header, Title, BenefitsTable],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  protected homeTitle: string = "Link your business card to unlock cashback offers";
 }
