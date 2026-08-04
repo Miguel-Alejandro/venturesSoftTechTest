6da3e4ff3fcb5e6001a8be159c08aa55921c175e
miguel <miguel.arellano@jaak.ai>
2026-08-03 19:12:15 -0600

    setup project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        pathMatch: "full",
+        redirectTo: "home"
+    },
+    {
+        path:"home",
+        loadComponent: () => import("./pages/home/home").then((c) => c.Home)
+    },
+    {
+        path: "**",
+        pathMatch: "full",
+        redirectTo: "hime"
+    }
+];
